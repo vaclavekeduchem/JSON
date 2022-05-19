9ad5ac5389f789790b2fbc37c8a39b3171de117f
Petr Václavek <vaclavek@educhem.cz>
2022-05-19 10:41:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from prettytable.colortable import ColorTable, Themes
 
 
 def vypis_napovedu():
-    print("1) Vypiš žáky\t 2) Přidat žáka\t 3) Vypiš žáka")
+    print("1) Vypiš žáky\t 2) Přidat žáka\t 3) Vypiš žáka\t 4) Ukončit program")
 
 
 def nacist_json(soubor: str):
@@ -33,19 +33,28 @@ def upravit_json(hodnoty: dict, zaci: list, soubor: str):
 
 
 def vypis_zaka(zaci: list, id: int):
-    print(zaci)
+    zak = zaci[id-1]
 
+    print("============")
+    print(f"\tID: {zak['id']}\n Jméno: {zak['jmeno']}\n Příjmení: {zak['prijmeni']}\n Třída: {zak['trida']}")
+    print("============")
 
 
 if __name__ == "__main__":
     zaci = nacist_json("zaci.json")
     while True:
         vypis_napovedu()
-        match int(input("Vyberte jednu z možností: ")):
-            case 1:
-                vypis_tabulku(zaci["zaci"])
-            case 2:
-                upravit_json({"jmeno": input("Jméno: "), "prijmeni": input("Příjmení: "), "trida": input("Třída: ")},
-                             zaci["zaci"], "zaci.json")
-            case 3:
-                vypis_zaka(zaci["zaci"], 1)
+        try:
+            match int(input("Vyberte jednu z možností: ")):
+                case 1:
+                    vypis_tabulku(zaci["zaci"])
+                case 2:
+                    upravit_json({"jmeno": input("Jméno: "), "prijmeni": input("Příjmení: "), "trida": input("Třída: ")},
+                                 zaci["zaci"], "zaci.json")
+                case 3:
+                    vypis_zaka(zaci["zaci"], int(input("Zadej ID žáka: ")))
+                case 4:
+                    exit()
+        except ValueError:
+            print("Musíš zadat pouze čísla")
+
