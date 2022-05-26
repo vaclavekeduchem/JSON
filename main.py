338ebb54cc1ff08a20ffe9e2f2783b1cc3016bf2
Petr Václavek <vaclavek@educhem.cz>
2022-05-26 10:33:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def upravit_json(hodnoty: dict, zaci: list, soubor: str):
 
 
 def vypis_zaka(zaci: list, id: int):
-    zak = zaci[id-1]
+    zak = zaci[id - 1]
 
     print("============")
     print(f"\tID: {zak['id']}\n Jméno: {zak['jmeno']}\n Příjmení: {zak['prijmeni']}\n Třída: {zak['trida']}")
@@ -49,12 +49,12 @@ if __name__ == "__main__":
                 case 1:
                     vypis_tabulku(zaci["zaci"])
                 case 2:
-                    upravit_json({"jmeno": input("Jméno: "), "prijmeni": input("Příjmení: "), "trida": input("Třída: ")},
-                                 zaci["zaci"], "zaci.json")
+                    upravit_json(
+                        {"jmeno": input("Jméno: "), "prijmeni": input("Příjmení: "), "trida": input("Třída: ")},
+                        zaci["zaci"], "zaci.json")
                 case 3:
                     vypis_zaka(zaci["zaci"], int(input("Zadej ID žáka: ")))
                 case 4:
                     exit()
         except ValueError:
             print("Musíš zadat pouze čísla")
-
